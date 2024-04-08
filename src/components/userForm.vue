5c4204982bb0ccf14091a87c8bf026174d509a91
tanvir35web <islammdtanvirul@gmail.com>
2024-04-08 14:56:44 +0600

    form functional done

--- src/components/userForm.vue
+++ src/components/userForm.vue
@@ -9,7 +9,12 @@
                     <hr />
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="text" id="email" class="form-control" />
+                        <input
+                            type="text"
+                            id="email"
+                            class="form-control"
+                            v-model="userData.email"
+                        />
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
@@ -17,11 +22,17 @@
                             type="password"
                             id="password"
                             class="form-control"
+                            v-model="userData.password"
                         />
                     </div>
                     <div class="form-group">
                         <label for="age">Age</label>
-                        <input type="number" id="age" class="form-control" />
+                        <input
+                            type="number"
+                            id="age"
+                            class="form-control"
+                            v-model="userData.age"
+                        />
                     </div>
                 </div>
             </div>
@@ -31,7 +42,12 @@
                 >
                     <label for="message">Message</label><br />
 
-                    <textarea id="message" rows="5" class="form-control">
+                    <textarea
+                        id="message"
+                        rows="5"
+                        class="form-control"
+                        v-model="userData.massage"
+                    >
                     </textarea>
                 </div>
             </div>
@@ -45,16 +61,36 @@
                                 type="checkbox"
                                 id="sendmail"
                                 value="SendMail"
+                                v-model="emailType"
                             />
                             Send Mail
                         </label>
-                        <label for="sendInfomail">
+                        <label for="eduMail">
                             <input
                                 type="checkbox"
-                                id="sendInfomail"
-                                value="SendInfoMail"
+                                id="eduMail"
+                                value="EduMail"
+                                v-model="emailType"
                             />
-                            Send Infomail   
+                            Send Edu Mail
+                        </label>
+                        <label for="businessMail">
+                            <input
+                                type="checkbox"
+                                id="businessMail"
+                                value="BusinessMail"
+                                v-model="emailType"
+                            />
+                            Send Business Mail
+                        </label>
+                        <label for="sendInfoMail">
+                            <input
+                                type="checkbox"
+                                id="sendInfoMail"
+                                value="SendInfoMail"
+                                v-model="emailType"
+                            />
+                            Send InfoMail
                         </label>
                     </div>
                 </div>
@@ -64,20 +100,38 @@
                     class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group"
                 >
                     <label for="male">
-                        <input type="radio" id="male" value="Male" /> Male
+                        <input
+                            type="radio"
+                            id="male"
+                            value="Male"
+                            v-model="gender"
+                        />
+                        Male
                     </label>
                     <label for="female">
-                        <input type="radio" id="female" value="Female" /> Female
+                        <input
+                            type="radio"
+                            id="female"
+                            value="Female"
+                            v-model="gender"
+                        />
+                        Female
                     </label>
                 </div>
             </div>
-            <div class="row"> 
+            <div class="row">
                 <div
                     class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group"
                 >
                     <label for="priority">Priority</label>
-                    <select id="priority" class="form-control">
-                        <option></option>
+                    <select
+                        id="priority"
+                        class="form-control"
+                        v-model="priority"
+                    >
+                        <option v-for="priority in priorities" :key="priority">
+                            {{ priority }}
+                        </option>
                     </select>
                 </div>
             </div>
@@ -86,12 +140,14 @@
                 <div
                     class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
                 >
-                    <button class="btn btn-primary">Submit!</button>
+                    <button class="btn btn-primary" @click.prevent="handleSubmit">
+                        Submit!
+                    </button>
                 </div>
             </div>
         </form>
         <hr />
-        <div class="row">
+        <div class="row" v-if="isSubmit">
             <div
                 class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
             >
@@ -100,17 +156,19 @@
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Mail:</p>
-                        <p>Password:</p>
-                        <p>Age:</p>
-                        <p>Message:</p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Age: {{ userData.age }}</p>
+                        <p>Message: {{ userData.massage }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="email in emailType" :key="email">
+                                {{ email }}
+                            </li>
                         </ul>
-                        <p>Gender:</p>
-                        <p>Priority:</p>
-                        <p>Switched:</p>
+                        <p>Gender: {{ gender }}</p>
+                        <p>Priority: {{ priority }}</p>
+                        <p>Switched: {{ isSubmit }}</p>
                     </div>
                 </div>
             </div>
@@ -120,10 +178,27 @@
 
 <script>
 export default {
-    data(){
+    data() {
         return {
-
-        }
+            userData: {
+                email: "",
+                password: "",
+                age: "",
+                massage: "",
+            },
+            emailType: [],
+            gender: "Male",
+            priorities: ["High", "Medium", "Low", "Normal"],
+            priority: "Medium",
+            isSubmit: false,
+        };
+    },
+    methods: {
+        handleSubmit() {
+            this.isSubmit = true;
+            console.log(this.userData);
+        },
     },
 };
+    
 </script>
