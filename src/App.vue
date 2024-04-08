3e6dd0ac546fa6cbca4fd5741f0395e94e199e87
tanvir35web <islammdtanvirul@gmail.com>
2024-04-08 13:11:59 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <userForm/>
+    </div>
+</template>
+
+<script>
+import userForm from "./components/userForm.vue";
+
+export default {
+    components: {
+        userForm
+    },
+};
+</script>
